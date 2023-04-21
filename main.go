47fe6bc258d7a2ccc575a0df2de4465aa436d301
Anon4Now <tutkoda@gmail.com>
2023-04-21 16:30:00 -0400

    added some content to test out json parsing

--- main.go
+++ main.go
@@ -1,11 +1,71 @@
 package main
 
 import (
-	// "log"
-	// "os"
 	"encoding/json"
+	"fmt"
+
+	// "io/ioutil"
+	"log"
+	"os"
 )
 
+type Projects struct {
+	Projects []Project `json:"projects"`
+}
+
+type Project struct {
+	Name string `json:"name"`
+	SubDirs SubDir1 `json:"subDir1"`
+}
+
+type SubDir1 struct {
+	Name string `json:"name"`
+	SubDir2 SubDir2 `json:"subDir2"`
+}
+
+type SubDir2 struct {
+	Name string `json:"name"`
+	SubDir3 SubDir3 `json:"subDir3"`
+}
+
+type SubDir3 struct {
+	Name string `json:"name"`
+	SubDir4 SubDir4 `json:"subDir4"`
+}
+
+type SubDir4 struct {
+	Name string `json:"name"`
+	SubDir5 SubDir5 `json:"subDir5"`
+}
+
+type SubDir5 struct {
+	Name string `json:"name"`
+}
+
+
+func readFile() {
+	data, err := os.ReadFile("project.json")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var projects Projects
+
+	json.Unmarshal(data, &projects)
+	
+
+	// for i := 0; i < len(projects.Projects); i++ {
+
+		
+	// }
+
+
+	// type_of := fmt.Sprintf("%T", data)
+	// fmt.Println(type_of)
+
+}
+
 func main() {
-	...
+	readFile()
 }
