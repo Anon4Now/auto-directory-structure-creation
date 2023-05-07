66d1641841c7f3380ddc07d32628761d40af8c06
Anon4Now <tutkoda@gmail.com>
2023-05-07 16:42:22 -0400

    update build shell script for multi-file

--- build-bin.sh
+++ build-bin.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 rm -r dist/
-env GOOS=linux GOARCH=amd64 go build -ldflags "-s -w" -o ./dist/main ./cmd/main.go
+env GOOS=linux GOARCH=amd64 go build -ldflags "-s -w" -o ./dist/main ./cmd/*.go
