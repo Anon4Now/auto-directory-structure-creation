eda59a51602dd74ac0305da90ee4bd238fcaa96c
Anon4Now <tutkoda@gmail.com>
2023-05-07 16:41:05 -0400

    moved code out of this file for more readability

--- main.go
+++ main.go
@@ -1,43 +1,12 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"io/fs"
 	"log"
 	"os"
-	"strings"
 )
 
-// *****MOVE SOME OF THIS TO OTHER FILES*****
-
-func parseArgs() []string {
-	/* 
-	Function that will parse CLI args from user
-	*/
-	var dirName string
-	var dirPath string
-	var branches string
-	var help bool
-
-	flag.StringVar(&dirName, "d", "default", "(optional) Provide a project name")
-	flag.StringVar(&dirPath, "p", "./", "(optional) Provide a path for creation of file structure")
-	flag.StringVar(&branches, "b", "all", "(optional) Choose whether to create with just EPT or IPT second-level directory")
-	flag.BoolVar(&help, "help", false, "Help")
-	flag.Parse()
-
-	if help {
-		flag.PrintDefaults()
-	}
-	
-	if branches != "all" {
-		if branches != "EPT" && branches != "IPT" {
-			log.Fatal("[-] Bad input for -b, please enter either 'EPT' or 'IPT' : -h for help")
-		}
-	}
-	argSlice := []string{dirName, dirPath, branches}
-	return argSlice
-}
 
 func makeDirCall(path string, method string) {
 	/*
@@ -57,100 +26,6 @@ func makeDirCall(path string, method string) {
 	}
 }
 
-type DirectoryStructure struct {
-	/*
-	Struct that contains attrs and methods related to the file structure creation.
-	*/
-
-	// Instantiate Struct
-	projectPath string
-	args []string
-
-	// Dynamically Updated
-	createdPaths []string
-}
-
-func (d *DirectoryStructure) getDataAndResetSlice() []string {
-	/*
-	Method that will return the data from the struct slice attr and set the slice to nil.
-	*/
-	data := d.createdPaths
-	d.createdPaths = nil
-	return data
-}
-
-func (d *DirectoryStructure) createSecondLevel() {
-	/*
-	Method that will build the 'second' level of the directory structure.
-	*/
-	secondLevel := []string{"EPT", "IPT"}
-
-	// checks for the cli passed from the user around how much to build 
-	// NEED TO TEST THE -b PARAMS
-	if d.args[2] == "all" {
-		for _, a := range secondLevel {
-			path := fmt.Sprintf("%v/%v", d.projectPath, a)
-			d.createdPaths = append(d.createdPaths, path)
-			makeDirCall(path, "mkdirall")
-		}
-	} else {
-		var path string
-
-		if d.args[2] == "EPT" {
-			path = fmt.Sprintf("%v/%v", d.projectPath, secondLevel[0])
-			d.createdPaths = append(d.createdPaths, path)
-		} else if d.args[2] == "IPT" {
-			path = fmt.Sprintf("%v/%v", d.projectPath, secondLevel[1])
-			d.createdPaths = append(d.createdPaths, path)
-		}
-	
-		makeDirCall(path, "mkdirall")
-	}
-
-	log.Println("[+] Successfully created second-level of directory.")
-}
-
-func (d *DirectoryStructure) createThirdLevel() {
-	/*
-	Method that will build the 'third' level of the directory structure.
-	*/
-	thirdLevel := []string{"evidence", "logs", "scans", "scope", "tools"}
-
-	pathData := d.getDataAndResetSlice()
-	fmt.Println(pathData)
-
-	for _, p := range pathData{
-		for _, a := range thirdLevel {
-			path := fmt.Sprintf("%v/%v", p, a)
-			d.createdPaths = append(d.createdPaths, path)
-			makeDirCall(path, "mkdirall")
-		}
-	}
-
-	log.Println("[+] Successfully created third-level of directory.")
-}
-
-func (d *DirectoryStructure) createFourthLevel() {
-	/*
-	Method that will build the 'fourth' level of the directory structure.
-	*/
-	fourthLevel := []string{"credentials", "data", "screenshots"}
-
-	pathData := d.getDataAndResetSlice()
-
-	for _, p := range pathData {
-		for _, a := range fourthLevel {
-			if strings.Contains(p, "evidence") {
-				path := fmt.Sprintf("%v/%v", p, a)
-				makeDirCall(path, "mkdirall")
-				continue
-			}
-		}
-	}
-
-	log.Println("[+] Successfully created fourth-level of directory.")
-
-}
 
 func main() {
 	args := parseArgs()
